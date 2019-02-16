c681908794754e883ee5369dc8b18ad0e92bc25a
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-02-16 18:44:28 -0500

    Update set-nvidia.sh

--- switch/set-nvidia.sh
+++ switch/set-nvidia.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 echo 'Removing intel only mode setup......'
 rm -rf /etc/X11/xorg.conf.d/99-intel.conf
 rm -rf /etc/modprobe.d/99-intel.conf
