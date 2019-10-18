0a1265ec9564d570cfc28a2733c99ae2c441c959
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-10-18 12:10:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,6 +37,10 @@ rm -rf /etc/X11/mhwd.d/nvidia.conf
 rm -rf /etc/X11/mhwd.d/nvidia.conf.nvidia-xconfig-original
 echo 'rm -rf /etc/X11/mhwd.d/nvidia.conf*'
 rm -rf /etc/X11/xorg.conf.d/90-mhwd.conf
+rm -rf /etc/X11/xorg.conf.d/nvidia.conf
+rm -rf /etc/X11/xorg.conf.d/optimus.conf
+rm -rf /etc/X11/xorg.conf.d/xorg.conf
+rm -rf /etc/X11/xorg.conf.d/20-intel.conf
 echo 'rm -rf /etc/X11/xorg.conf.d/90-mhwd.conf'
 rm -rf /etc/modprobe.d/mhwd-gpu.conf
 rm -rf /etc/modprobe.d/optimus.conf
